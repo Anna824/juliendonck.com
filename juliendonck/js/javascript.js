d4c8d4e3a146977e5e12bfcc29bb496af1585512
Julien Donck <donckjulien@gmail.com>
2013-04-11 13:00:46 +0200

    making the front end responsive

--- juliendonck/js/javascript.js
+++ juliendonck/js/javascript.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
 
   $('.scrollup').click(function(){
     $("html, body").animate({ scrollTop: 0 }, 400);
-    console.log("wuw");
     return false;
   });
 
@@ -24,11 +23,7 @@ $(document).ready(function() {
     } else {
 
       $('img.cross').attr("src","img/cross-light@2x.png");
-      $('p.top').css('color','#f9f9f9');
       $('a.hover').hover(function() {
-        $('img.cross').attr("src","img/arrow-light@2x.png");
-        $('p.top').css('visibility', 'visible');
-      }, function() {
         $('img.cross').attr("src","img/cross-light@2x.png");
         $('p.top').css('visibility', 'hidden');
       });
@@ -40,35 +35,43 @@ $(document).ready(function() {
 
 
   $('a.twitter').hover(function() {
-    $("header").css('background-color', 'rgba(0,171,240,.95)');
-    $(".twitter img").css({ opacity: 0.85 });
+    $("header").css('background-color', 'rgb(0,171,240)');
+    $(".twitter img").css({ opacity: 0.9 });
   }, function() {
-    $("header").css('background-color', 'rgba(0,163,204,.95)');
+    $("header").css('background-color', '#00A3CC');
     $(".twitter img").css({ opacity: 1 });
   });
 
   $('a.vimeo').hover(function() {
-    $("header").css('background-color', 'rgba(27,182,236,.95)');
+    $("header").css('background-color', 'rgb(27,182,236)');
+    $(".vimeo img").css({ opacity: 0.9 });
   }, function() {
-    $("header").css('background-color', 'rgba(0,163,204,.95)');
+    $("header").css('background-color', '#00A3CC');
+    $(".vimeo img").css({ opacity: 1 });
   });
 
   $('a.dribbble').hover(function() {
-    $("header").css('background-color', 'rgba(226,81,137,.95)');
+    $("header").css('background-color', 'rgb(226,81,137)');
+    $(".dribbble img").css({ opacity: 0.9 });
   }, function() {
-    $("header").css('background-color', 'rgba(0,163,204,.95)');
+    $("header").css('background-color', '#00A3CC');
+    $(".dribbble img").css({ opacity: 1 });
   });
 
   $('a.tumblr').hover(function() {
-    $("header").css('background-color', 'rgba(58,88,118,.95)');
+    $("header").css('background-color', 'rgb(58,88,118)');
+    $(".tumblr img").css({ opacity: 0.9 });
   }, function() {
-    $("header").css('background-color', 'rgba(0,163,204,.95)');
+    $("header").css('background-color', '#00A3CC');
+    $(".tumblr img").css({ opacity: 1 });
   });
 
   $('a.instagram').hover(function() {
-    $("header").css('background-color', 'rgba(81,127,164,.95)');
+    $("header").css('background-color', 'rgb(81,127,164)');
+    $(".instagram img").css({ opacity: 0.9 });
   }, function() {
-    $("header").css('background-color', 'rgba(0,163,204,.95)');
+    $("header").css('background-color', '#00A3CC');
+    $(".instagram img").css({ opacity: 1 });
   });
 });
 
