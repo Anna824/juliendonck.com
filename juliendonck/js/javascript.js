7d839f76c9823b94be9ab1eb57d21bdd41f20996
Julien Donck <donckjulien@gmail.com>
2013-04-12 11:43:11 +0200

    tweak color fade

--- juliendonck/js/javascript.js
+++ juliendonck/js/javascript.js
@@ -32,42 +32,42 @@ $(document).ready(function() {
 
 
   $('a.twitter').hover(function() {
-    $("header").animate({backgroundColor: 'rgb(0,171,240)'},300);
+    $("header").animate({backgroundColor: 'rgb(0,171,240)'},200);
     $(".twitter img").css({ opacity: 0.9 });
   }, function() {
-    $("header").animate({backgroundColor: '#00A3CC'},300);
+    $("header").animate({backgroundColor: '#00A3CC'},200);
     $(".twitter img").css({ opacity: 1 });
   });
 
   $('a.vimeo').hover(function() {
-    $("header").animate({backgroundColor: 'rgb(27,182,236)'},300);
+    $("header").animate({backgroundColor: 'rgb(27,182,236)'},200);
     $(".vimeo img").css({ opacity: 0.9 });
   }, function() {
-    $("header").animate({backgroundColor: '#00A3CC'},300);
+    $("header").animate({backgroundColor: '#00A3CC'},200);
     $(".vimeo img").css({ opacity: 1 });
   });
 
   $('a.dribbble').hover(function() {
-    $("header").animate({backgroundColor: 'rgb(226,81,137)'},300);
+    $("header").animate({backgroundColor: 'rgb(226,81,137)'},200);
     $(".dribbble img").css({ opacity: 0.9 });
   }, function() {
-    $("header").animate({backgroundColor: '#00A3CC'},300);
+    $("header").animate({backgroundColor: '#00A3CC'},200);
     $(".dribbble img").css({ opacity: 1 });
   });
 
   $('a.tumblr').hover(function() {
-    $("header").animate({backgroundColor: 'rgb(58,88,118)'},300);
+    $("header").animate({backgroundColor: 'rgb(58,88,118)'},200);
     $(".tumblr img").css({ opacity: 0.9 });
   }, function() {
-    $("header").animate({backgroundColor: '#00A3CC'},300);
+    $("header").animate({backgroundColor: '#00A3CC'},200);
     $(".tumblr img").css({ opacity: 1 });
   });
 
   $('a.instagram').hover(function() {
-    $("header").animate({backgroundColor: '#517FA4'},300);
+    $("header").animate({backgroundColor: '#517FA4'},200);
     $(".instagram img").css({ opacity: 0.9 });
   }, function() {
-    $("header").animate({backgroundColor: '#00A3CC'},300);
+    $("header").animate({backgroundColor: '#00A3CC'},200);
     $(".instagram img").css({ opacity: 1 });
   });
 
